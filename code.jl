a842bc0ec19814f8a672359312238ee26c31356a
Alexandre Renchon <arenchon@sampo.gps.caltech.edu>
2023-03-27 08:31:40 -0700

    1st commit

---
+++ code.jl
@@ -0,0 +1,27 @@
+using JSServe, WGLMakie #, MakieCore, Deno_jll
+
+function mkfig()
+  fig = Figure()
+  ax = Axis(fig[1,1])
+  plot!(ax, rand(2), rand(2))
+  fig
+  return fig
+end
+
+Sampo_IP = "131.215.103.129"
+
+JSServe.Server(App(DOM.div(mkfig())), Sampo_IP, 9384)
+
+
+# my_app = App() do session::Session
+#  fig = mkfig()
+#  return DOM.div(fig)
+# end
+# Sampo_IP = "131.215.103.129"
+# JSServe.Server(my_app, Sampo_IP, 9384)
+# server = JSServe.get_server()
+# route!(server, "/JSServe" => my_app)
+
+# JSServe.Server(App(DOM.div("hello world")), "131.215.103.129", 9384)
+
+
